b94799cbaeea2b26e3e0db1c67a2c11f636bf7e2
iddev5 <ayushtechportal@gmail.com>
2021-08-09 23:31:08 +0530

    Minor changes to SDL test

--- SDL-test/src/main.zig
+++ SDL-test/src/main.zig
@@ -2,6 +2,11 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
+const Player = struct {
+    x: i32,
+    y: i32,
+};
+
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Could not initialize SDL: %s\n", c.SDL_GetError());
@@ -11,18 +16,17 @@ pub fn main() !void {
 
     const window = c.SDL_CreateWindow("Test", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 800, 600, 0) orelse {
         c.SDL_Log("Could not create window: %s\n", c.SDL_GetError());
-        return error.SDLInitFailed;
+        return error.SDLWindowFailed;
     };
     defer c.SDL_DestroyWindow(window);
 
     const renderer = c.SDL_CreateRenderer(window, -1, 0) orelse {
         c.SDL_Log("Could not create renderer: %s\n", c.SDL_GetError());
-        return error.SDLInitFailed;
+        return error.SDLRendererFailed;
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    var pos_x: i32 = 10;
-    var pos_y: i32 = 10;
+    var player: Player = .{ .x = 10, .y = 10, };
 
     var is_running: bool = true;
     var event: c.SDL_Event = undefined;
@@ -32,10 +36,11 @@ pub fn main() !void {
                 c.SDL_QUIT => { is_running = false; },
                 c.SDL_KEYDOWN => {
                     switch (event.key.keysym.sym) {
-                        c.SDLK_w => { pos_y -= 5; },
-                        c.SDLK_s => { pos_y += 5; },
-                        c.SDLK_a => { pos_x -= 5; },
-                        c.SDLK_d => { pos_x += 5; },
+                        c.SDLK_w => { player.y -= 5; },
+                        c.SDLK_s => { player.y += 5; },
+                        c.SDLK_a => { player.x -= 5; },
+                        c.SDLK_d => { player.x += 5; },
+                        c.SDLK_ESCAPE => { is_running = false; },
                         else => { },
                     }
                 },
@@ -46,7 +51,7 @@ pub fn main() !void {
         _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         _ = c.SDL_RenderClear(renderer);
         _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        _ = c.SDL_RenderFillRect(renderer, &c.SDL_Rect{.x=pos_x, .y=pos_y, .w=100, .h=100});
+        _ = c.SDL_RenderFillRect(renderer, &c.SDL_Rect{.x=player.x, .y=player.y, .w=100, .h=100});
 
         c.SDL_RenderPresent(renderer);
     }
